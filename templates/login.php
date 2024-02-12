15a7c70b6a7fd083bf26ae926ec4f05f38d9289e
Evann45 <evann45yang@gmail.com>
2024-02-12 18:30:54 +0100

    A jour login

--- templates/login.php
+++ templates/login.php
@@ -3,24 +3,26 @@ $title = "Connexion";
 $h1 = "Connexion";
 ob_start();
 ?>
-
-<form action="/login" method="post">
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required>
+<div class="container">
+    <div class="form-container">
+        <form action="/login" method="post">
+            <div class="form-group">
+                <label class="labelRegister" for="email">Email</label>
+                <input class="inputRegister" type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label class="labelRegister" for="mdp">Mot de passe</label>
+                <input class="inputRegister" type="password" class="form-control" id="mdp" name="mdp" required>
+            </div>
+            <?php 
+            if (isset($error)) {
+                echo "<p>" . $error . "</p>";
+            }
+            ?>
+            <button type="submit" class="btn btn-primary">Se connecter</button>
+        </form>
     </div>
-    <div class="form-group">
-        <label for="mdp">Mot de passe</label>
-        <input type="password" class="form-control" id="mdp" name="mdp" required>
-    </div>
-    <?php 
-    if (isset($error)) {
-        echo "<p>" . $error . "</p>";
-    }
-    ?>
-    <button type="submit" class="btn btn-primary">Se connecter</button>
-</form>
-
+</div>
 <?php
 $content = ob_get_clean();
 include 'connectBaseLayout.php';
