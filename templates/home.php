337610527b283837c6887a859ab42c4a9a44477d
tavign <theo.avignon271104@gmail.com>
2024-02-15 10:31:45 +0100

    ajoute style base et style section de recherche

--- templates/home.php
+++ templates/home.php
@@ -1,10 +1,13 @@
 <?php
-session_start();
-$title = "Home";
+$title = "Librairie";
 ob_start();
 ?>
-
-<div class="container-sm mt-5">
+<link rel="stylesheet" href="./static/librairie.css">
+<form class="chercher_album" action="/home" method="get">
+    <input type="text" name="recherche" placeholder="Rechercher un album">
+    <button type="submit">Rechercher</button>
+</form>
+<div class="albums-wrapper">
     <?php foreach ($albums as $album) {
         echo $album->render();
     } ?>
